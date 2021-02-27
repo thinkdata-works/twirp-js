c1a6dba073d4f0d2f6b69f6ee89e3d01cc72abcb
Bronek Szulc <bronek@thinkdataworks.com>
2021-02-27 17:07:22 -0500

    fixing node-fetch so that it works properly with webpack bundling

--- src/index-node.js
+++ src/index-node.js
@@ -4,8 +4,14 @@
 var client = require("./client");
 var fetch = require("node-fetch");
 
+// requiring this file directly in node will return node-fetch as a function
+// bundling with webpack may return node-fetch as an object with the function nested under "default"
+const fetchFn = Object.prototype.toString.call(fetch) === '[object Function]'
+    ? fetch
+    : fetch.default;
+
 const serialize = msg => Buffer.from(msg.serializeBinary());
 const deserialize = responseType => res => res.arrayBuffer()
     .then(b => responseType.deserializeBinary(new Uint8Array(b)).toObject());
 
-module.exports = client.clientFactory(fetch, serialize, deserialize);
+module.exports = client.clientFactory(fetchFn, serialize, deserialize);
